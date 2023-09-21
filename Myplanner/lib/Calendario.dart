a0f5cf47868d0ca120cfb48fee66b1616f19e762
NilsonDeon <nilsondeon01@gmail.com>
2023-09-21 10:23:08 -0300

    add-calendar

--- Myplanner/lib/Calendario.dart
+++ Myplanner/lib/Calendario.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Calendario extends StatelessWidget {
   final String texto;
@@ -9,8 +10,47 @@ class Calendario extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text(texto),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2023, 1, 1),
+              lastDay: DateTime.utc(2023, 12, 31),
+              focusedDay: DateTime.now(),
+
+              calendarStyle: const CalendarStyle(
+                isTodayHighlighted: true,
+                selectedDecoration: BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+                selectedTextStyle: TextStyle(color: Colors.white),
+                todayDecoration: BoxDecoration(
+                  color: Colors.grey,
+                  shape: BoxShape.circle,
+                ),
+                defaultDecoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+                weekendDecoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+              ),
+
+              daysOfWeekStyle: const DaysOfWeekStyle(
+                weekendStyle: TextStyle(color: Colors.black),
+              ),
+
+              headerStyle: const HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
+              ),
+
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
