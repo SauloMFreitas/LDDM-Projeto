233eeddecb56326597752c04de77123bd0ee0add
NilsonDeon <nilsondeon01@gmail.com>
2023-09-24 17:45:01 -0300

    pet-centralized

--- Myplanner/lib/Pet.dart
+++ Myplanner/lib/Pet.dart
@@ -3,27 +3,23 @@ import 'package:percent_indicator/percent_indicator.dart';
 import 'dart:math';
 
 class Pet extends StatefulWidget {
-
   final int? xpAtual;
-  Pet({this.xpAtual});
+  const Pet({this.xpAtual});
 
   @override
+  // ignore: library_private_types_in_public_api
   _Pet createState() => _Pet();
 }
 
-
 class _Pet extends State<Pet> {
-
   int _nivelAtual = 0;
 
   TextEditingController _texto = TextEditingController();
 
   void _updateNivel() {
-
     if (widget.xpAtual != null && widget.xpAtual! >= 0) {
       _nivelAtual = (widget.xpAtual! / 100).floor();
     }
-
   }
 
   double _getPercent() {
@@ -41,29 +37,40 @@ class _Pet extends State<Pet> {
   Widget build(BuildContext context) {
     _updateNivel();
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          children: <Widget>[
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text("Jorjinho"),
+              const SizedBox(height: 16),
 
-            Text("Jorjinho"),
+              Image.asset('images/Pet_$_nivelAtual.png'),
+              const SizedBox(height: 16),
 
-            Image.asset('images/Pet_${_nivelAtual}.png'),
+              Text('${widget.xpAtual! % 100}/100'),
+              const SizedBox(height: 16), 
 
-            Text('${widget.xpAtual!%100}/100'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  LinearPercentIndicator(
+                    width: 200.0,
+                    lineHeight: 14.0,
+                    percent: _getPercent(),
+                    backgroundColor: Colors.white,
+                    progressColor: Colors.blue,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
 
-            LinearPercentIndicator(
-                width: 200.0,
-                lineHeight: 14.0,
-                percent: _getPercent(),
-                backgroundColor: Colors.grey,
-                progressColor: Colors.blue,
-            ),
+              Text('Nivel: $_nivelAtual'),
 
-            Text('Nivel: ${_nivelAtual}')
-
-          ],
-        ),
+            ],
+          )
+        ],
       ),
     );
   }
