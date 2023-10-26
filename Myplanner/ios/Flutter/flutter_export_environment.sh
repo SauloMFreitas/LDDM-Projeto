69e923fe03d73733ddf28aa7587eee8a48ffcb40
Gabriel Vargas Bento de Souza <105235647+GabrielVargasBS@users.noreply.github.com>
2023-10-26 15:46:26 -0300

    Update Lista (cor, filtro e BD)

--- Myplanner/ios/Flutter/flutter_export_environment.sh
+++ Myplanner/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/gabriel/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/gabriel/Desktop/CC-PUC/4periodo/LDDM/MyPlanner/LDDM-Projeto-main/myplanner"
+export "FLUTTER_APPLICATION_PATH=/home/gabriel/Desktop/CC-PUC/4periodo/myplanner"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
