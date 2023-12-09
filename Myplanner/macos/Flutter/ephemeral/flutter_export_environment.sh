6c18dbe535d5923eae2dce85e33e10eca8fb4f0a
SauloMFreitas <saulo.freitas.1400518@sga.pucminas.br>
2023-12-09 19:34:04 -0300

    correção de incoerências

--- Myplanner/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ Myplanner/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/gabriel/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/gabriel/Desktop/CC-PUC/4periodo/LDDM/MyPlanner/LDDM-Projeto-main/myplanner"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\saulo\OneDrive\Documentos\GitHub\LDDM-Projeto\Myplanner"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
